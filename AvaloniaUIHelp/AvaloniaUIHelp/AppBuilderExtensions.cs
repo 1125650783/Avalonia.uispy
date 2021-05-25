03b1c7c019557e1ece6a1e1d9dda17986a0dcbcd
yzf <mes16@Mesign.com>
2021-05-25 15:04:33 +0800

    调试增加URi地址

---
+++ AvaloniaUIHelp/AvaloniaUIHelp/AppBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace AvaloniaUIHelp
+{
+    public static class AppBuilderUIExtensions
+    {
+        public static TAppBuilder UseAvaloniaUIHelp<TAppBuilder>(this TAppBuilder builder)
+            where TAppBuilder : AppBuilderBase<TAppBuilder>, new()
+        {
+            try
+            {
+                if (Debugger.IsAttached)
+                {
+                    AvaloniaUIHelp.Init(out _);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return builder;
+        }
+    }
+
+
+}
